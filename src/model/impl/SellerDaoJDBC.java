0963230a43fb32f01f631196a7aa5fb1b78323fd
Thiago <thiago.silveira@projuris.com.br>
2020-12-19 15:31:38 -0300

    TextField and DatePicker

--- src/model/impl/SellerDaoJDBC.java
+++ src/model/impl/SellerDaoJDBC.java
@@ -137,7 +137,7 @@ public class SellerDaoJDBC implements SellerDao {
         seller.setId(resultSet.getInt("Id"));
         seller.setName(resultSet.getString("Name"));
         seller.setEmail(resultSet.getString("Email"));
-        seller.setBirthDate(resultSet.getDate("BirthDate"));
+        seller.setBirthDate(new java.util.Date(resultSet.getTimestamp("BirthDate").getTime()));
         seller.setBaseSalary(resultSet.getDouble("BaseSalary"));
         seller.setDepartment(department);
         return seller;
--- src/util/Utils.java
+++ src/util/Utils.java
@@ -2,11 +2,15 @@ package util;
 
 import javafx.event.ActionEvent;
 import javafx.scene.Node;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.stage.Stage;
+import javafx.util.StringConverter;
 
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
 import java.util.Locale;
 
@@ -43,7 +47,7 @@ public class Utils {
         });
     }
 
-    public static<T> void formatTableColumnDouble(TableColumn<T, Double> tableColumn, int decimalPlaces) {
+    public static <T> void formatTableColumnDouble(TableColumn<T, Double> tableColumn, int decimalPlaces) {
         tableColumn.setCellFactory(column -> {
             TableCell<T, Double> cell = new TableCell<T, Double>() {
                 @Override
@@ -56,7 +60,36 @@ public class Utils {
                         setText(String.format("%." + decimalPlaces + "f", item));
                     }
                 }
-            }; return cell;
+            };
+            return cell;
+        });
+    }
+
+    public static void formatDatePicker(DatePicker datePicker, String format) {
+        datePicker.setConverter(new StringConverter<LocalDate>() {
+            DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern(format);
+
+            {
+                datePicker.setPromptText(format.toLowerCase());
+            }
+
+            @Override
+            public String toString(LocalDate date) {
+                if (date != null) {
+                    return dateFormatter.format(date);
+                } else {
+                    return "";
+                }
+            }
+
+            @Override
+            public LocalDate fromString(String string) {
+                if (string != null && !string.isEmpty()) {
+                    return LocalDate.parse(string, dateFormatter);
+                } else {
+                    return null;
+                }
+            }
         });
     }
 }
